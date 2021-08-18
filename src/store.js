39cacaaa42ed9b752cf28d14d25f54ea66807d10
Matongo Mulindi <mmatongo_@hotmail.com>
2021-08-18 14:10:56 +0200

    add logic to store to crossout & update item status

--- src/store.js
+++ src/store.js
@@ -1,15 +1,43 @@
-Object.defineProperty(exports, '__esModule', {
-  value: true,
-});
-
-function toStorage(data) {
+export function toStorage(data) {
   localStorage.setItem('todo', JSON.stringify(data));
 }
 
-function fromStorage() {
+export function fromStorage() {
   const todoList = localStorage.getItem('todo');
   return JSON.parse(todoList);
 }
 
-exports.toStorage = toStorage;
-exports.fromStorage = fromStorage;
+function markTodoItem(index, value) {
+  const list = fromStorage();
+
+  list.forEach((item) => {
+    if (item.index === Number(index) || item.index === index.toString()) {
+      item.completed = value;
+    }
+  });
+
+  toStorage(list);
+}
+
+function updateTodo(todoItem) {
+  const checkbox = todoItem.children[0].children[0];
+  const checkboxIndex = checkbox.getAttribute('name').split('-')[1];
+
+  if (checkbox.checked) {
+    markTodoItem(checkboxIndex, true);
+    checkbox.nextElementSibling.style.textDecoration = 'line-through';
+  } else {
+    markTodoItem(checkboxIndex, false);
+    checkbox.nextElementSibling.style.textDecoration = 'none';
+  }
+}
+
+export function reloadStore() {
+  const todoItems = document.getElementsByClassName('todo-item');
+
+  [...todoItems].forEach((todoItem) => {
+    todoItem.children[0].children[0].addEventListener('change', () => {
+      updateTodo(todoItem);
+    });
+  });
+}
